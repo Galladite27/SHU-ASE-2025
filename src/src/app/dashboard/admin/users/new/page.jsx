818af06ebcf883c8613d23bfea14f3eb2f8da782
mahmoodharis540 <mahmoodharis540@gmail.com>
2025-11-18 11:33:43 +0000

    Updated - fixed broken appliacation.

--- src/src/app/dashboard/admin/users/new/page.jsx
+++
@@ -1,9 +0,0 @@
-import ClerkUserManager from "./client";
-
-export default function NewUserPage() {
-    return (
-        <main className="p-6 sm:p-8 bg-gray-100 min-h-screen">
-            <ClerkUserManager />
-        </main>
-    );
-}
--- src/src/lib/clerkadmin.js
+++
@@ -1,5 +0,0 @@
-
-import { createClerkClient } from "@clerk/backend";
-export const clerkAdmin = createClerkClient({ secretKey: 
-    'sk_test_QRBTz1ipiakDdvvBvUhs2utJxRqf0u4OtO4UejTtRq'
-});
