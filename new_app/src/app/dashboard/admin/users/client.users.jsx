b658b7f7bb7ec42f739b92d37bb9f20207de3e67
mahmoodharis540 <mahmoodharis540@gmail.com>
2025-12-01 18:06:11 +0000

    Very Sexy

--- new_app/src/app/dashboard/admin/users/client.users.jsx
+++ new_app/src/app/dashboard/admin/users/client.users.jsx
@@ -93,6 +93,8 @@ export default function UsersPageClient({ initialUsers }) {
           onClick={() => setShowCreateForm(!showCreateForm)}
           className="bg-purple-700 text-white px-4 py-2 rounded hover:bg-purple-900 w-full"
         >
+          {/* Added the new database from Theo because it didn't work before :/ */}
+          
           {showCreateForm ? "Close (X)" : "Create New User"}
         </button>
 
